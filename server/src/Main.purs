e38295c57f06fdccc96264e316933b229b7f101e
Ville Peurala <ville.peurala@gmail.com>
2017-09-10 14:13:21 +0300

    Output a message when listening to a port.

--- server/src/Main.purs
+++ server/src/Main.purs
@@ -1,13 +1,24 @@
 module Main where
 
-import Node.HTTP (HTTP, Request, Response, createServer)
+import Node.HTTP (HTTP, ListenOptions, Request, Response, createServer, listen)
 
 import Control.Applicative (pure)
 import Control.Bind (bind, discard)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 
+import Data.Maybe (Maybe(Nothing))
 import Data.Unit (Unit, unit)
+
+listenOptions :: ListenOptions
+listenOptions = {
+  hostname: "localhost",
+  port: 9700,
+  backlog: Nothing
+}
+
+listenCallback :: Eff (http :: HTTP, console :: CONSOLE) Unit
+listenCallback = log "Server listening on port 9700."
 
 respond :: forall eff. Request -> Response -> Eff (http :: HTTP | eff) Unit
 respond request response = pure unit
@@ -16,4 +27,5 @@ main :: Eff (console :: CONSOLE, http :: HTTP) Unit
 main = do
   log "Before creating server"
   server <- createServer respond
+  listen server listenOptions listenCallback
   log "Hello cockboys!"
