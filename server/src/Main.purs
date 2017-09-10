d19b71152e3e3f0be19be20f67c0a10f7735e90a
Ville Peurala <ville.peurala@gmail.com>
2017-09-10 13:41:26 +0300

    Do-nothing respond function

--- server/src/Main.purs
+++ server/src/Main.purs
@@ -1,14 +1,23 @@
 module Main where
 
-import Node.HTTP (Server(), createServer)
+import Node.HTTP (HTTP, Server(), Request, Response, createServer)
 
 import Partial.Unsafe (unsafeCrashWith)
 
+import Control.Applicative (pure)
 import Control.Bind (bind, discard)
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (log)
+
+import Data.Unit (Unit, unit)
+
+import Unsafe.Coerce (unsafeCoerce)
+
+respond :: forall eff. Request -> Response -> Eff (http :: HTTP | eff) Unit
+respond request response = pure unit
 
 main = do
   log "Before creating server"
-  server <- createServer (unsafeCrashWith "foo!")
+  server <- createServer respond
   log "Hello cockboys!"
 
