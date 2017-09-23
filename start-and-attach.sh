efa5771fe67115f05abe5fb31131aff01f264407
Ville Peurala <ville.peurala@gmail.com>
2017-09-24 00:41:09 +0300

    Add TODO

--- start-and-attach.sh
+++ start-and-attach.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 docker rm herigone-ps-latest
+# TODO Specify the server port here from a parameter or something
 docker container create -it --name herigone-ps-latest --user node --publish 9700:9700 vpeurala/herigone-ps:latest
 docker container start -ia herigone-ps-latest
