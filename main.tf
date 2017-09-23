32f2261cdc58125d732fb87b4e27ab88d8b09c21
Ville Peurala <ville.peurala@gmail.com>
2017-09-23 21:55:50 +0300

    Use a simpler Terraform configuration without the tf_aws_vpc module.

--- main.tf
+++ main.tf
@@ -2,10 +2,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-module "vpc" {
-    source = "github.com/terraform-community-modules/tf_aws_vpc"
-    name = "herigone-ps-vpc"
-    cidr = "10.0.0.0/16"
-    public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
-    azs = ["eu-west-1a", "eu-west-1b"]
+resource "aws_ecs_cluster" "herigone-ps-ecs-cluster" {
+  name = "herigone-ps-ecs-cluster"
 }
