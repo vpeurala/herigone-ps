36790d79f818ee422d2f69a4df441d0ff5970be7
Ville Peurala <ville.peurala@gmail.com>
2017-09-28 15:29:13 +0300

    4 passing unit tests for Environment module.

--- purescript-herigone-server/test/Main.purs
+++ purescript-herigone-server/test/Main.purs
@@ -45,6 +45,15 @@ main = run [consoleReporter] do
         liftEff turnOffConsoleLogCapturing
         logs `shouldEqual` ["Environment variable HERIGONE_SERVER_PORT set to a value of \"foo\" which could not be parsed as an integer. Using the default port of 9771."]
 
+      it "returns the port set in env variable HERIGONE_SERVER_PORT if it is valid" do
+        liftEff $ setEnv "HERIGONE_SERVER_PORT" "9772"
+        liftEff turnOnConsoleLogCapturing
+        port <- liftEff getHttpServerPort
+        port `shouldEqual` 9772
+        logs <- liftEff getCapturedConsoleLogs
+        liftEff turnOffConsoleLogCapturing
+        logs `shouldEqual` ["Environment variable HERIGONE_SERVER_PORT set to a value of 9772."]
+
 foreign import turnOnConsoleLogCapturing :: forall eff. Eff (console :: CONSOLE | eff) Unit
 
 foreign import getCapturedConsoleLogs :: forall eff. Eff (console :: CONSOLE | eff) (Array String)
