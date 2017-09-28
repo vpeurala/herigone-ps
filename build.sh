3dd79d55a9cb1f428233c4baf4d42a9c24fcac7e
Ville Peurala <ville.peurala@gmail.com>
2017-09-28 10:21:45 +0300

    Clean script for destroying all automatically downloadable stuff.

--- build.sh
+++ build.sh
@@ -1,1 +1,2 @@
+#!/usr/bin/env sh
 docker build -t vpeurala/herigone-ps:latest .
---
+++ clean.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env sh
+rm -rf purescript-herigone-common/.pulp-cache
+rm -rf purescript-herigone-common/bower_components
+rm -rf purescript-herigone-common/node_modules
+rm -rf purescript-herigone-common/output
+
+rm -rf purescript-herigone-server/.pulp-cache
+rm -rf purescript-herigone-server/bower_components
+rm -rf purescript-herigone-server/node_modules
+rm -rf purescript-herigone-server/output
