ff96be7cb58d5e5928cfba7aae6b48fef4147fc3
Ville Peurala <ville.peurala@gmail.com>
2017-09-28 13:22:06 +0300

    First unit test. Logging not tested yet.

--- purescript-herigone-server/test/Main.purs
+++ purescript-herigone-server/test/Main.purs
@@ -3,16 +3,18 @@ module Test.Main where
 import Test.Spec (describe, it)
 import Test.Spec.Assertions (shouldEqual)
 import Test.Spec.Reporter.Console (consoleReporter)
-import Test.Spec.Runner (run)
+import Test.Spec.Runner (RunnerEffects, run)
 
+import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 
-import Prelude (bind)
+import Prelude (Unit, bind)
 
 import Herigone.Environment (getHttpServerPort)
 
+main :: Eff (RunnerEffects ()) Unit
 main = run [consoleReporter] do
   describe "getHttpServerPort" do
     it "returns the default port if not set" do
       port <- liftEff getHttpServerPort
-      "foo" `shouldEqual` "foo"
+      port `shouldEqual` 9771
