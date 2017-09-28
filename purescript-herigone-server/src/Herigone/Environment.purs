e1d9c315bd3677c1a2c6dc4f0ac1ead17d280647
Ville Peurala <ville.peurala@gmail.com>
2017-09-28 12:36:17 +0300

    Formatting, remove unnecessary braces.

--- purescript-herigone-server/src/Herigone/Environment.purs
+++ purescript-herigone-server/src/Herigone/Environment.purs
@@ -7,23 +7,23 @@ import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Maybe as M
 
-import Prelude (bind, discard, pure, show, (<>))
+import Prelude (bind, discard, pure, show, ($), (<>))
 
 getHttpServerPort :: forall eff. Eff (process :: PROCESS, console :: CONSOLE | eff) Int
 getHttpServerPort = do
   environmentVariable <- lookupEnv "HERIGONE_SERVER_PORT"
   case environmentVariable of
     M.Nothing -> do
-      log ("Environment variable HERIGONE_SERVER_PORT not set.")
+      log $ "Environment variable HERIGONE_SERVER_PORT not set. Using the default port of " <> show defaultPort <> "."
       pure defaultPort
     M.Just environmentVariableValue -> do
       let parsedPort = Int.fromString environmentVariableValue
       case parsedPort of
         M.Nothing -> do
-          log ("Environment variable HERIGONE_SERVER_PORT set to a value of \"" <> environmentVariableValue <> "\" which could not be parsed as an integer.")
+          log $ "Environment variable HERIGONE_SERVER_PORT set to a value of \"" <> environmentVariableValue <> "\" which could not be parsed as an integer. Using the default port of " <> show defaultPort <> "."
           pure defaultPort
         M.Just portNumber -> do
-          log ("Environment variable HERIGONE_SERVER_PORT set to a value of " <> (show portNumber) <> ".")
+          log $ "Environment variable HERIGONE_SERVER_PORT set to a value of " <> show portNumber <> "."
           pure portNumber
 
 defaultPort :: Int
