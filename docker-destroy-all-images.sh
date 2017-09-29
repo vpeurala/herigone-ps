8359c375a09d55641fae0a0d3b47fe9adf06b321
Ville Peurala <ville.peurala@gmail.com>
2017-09-29 06:26:58 +0300

    Destroy also all images of vpeurala/herigone-ps except the latest one.

--- docker-destroy-all-images.sh
+++ docker-destroy-all-images.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env sh
-docker images --all | grep -v ubuntu | grep -v vpeurala/herigone-ps | tail -n +2 | awk '{print $3}' | xargs docker rmi -f
+docker images --all | grep -v ubuntu | grep -v -e 'vpeurala/herigone-ps.*latest' | tail -n +2 | awk '{print $3}' | xargs docker rmi -f
