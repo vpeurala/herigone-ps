8b89f09e365b03357451fbadbcf90ca6f0a62add
Ville Peurala <ville.peurala@gmail.com>
2017-09-29 06:20:38 +0300

    Do an AJAX call with purescript-affjax.

--- purescript-herigone-client/src/Main.purs
+++ purescript-herigone-client/src/Main.purs
@@ -1,9 +1,16 @@
 module Main where
 
-import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
+import Control.Monad.Eff.Class (liftEff)
+import Control.Monad.Aff (Fiber, launchAff)
+import Data.Either (Either(..))
+import Data.HTTP.Method (Method(..))
+import Network.HTTP.Affjax (AJAX, affjax, defaultRequest)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Prelude
+
+main :: forall eff. Eff (ajax :: AJAX, console :: CONSOLE | eff) (Fiber (ajax :: AJAX, console :: CONSOLE | eff) Unit)
+main = launchAff $ do
+  res <- affjax $ defaultRequest { url = "/associations", method = Left GET }
+  liftEff $ log $ "GET /associations response: " <> res.response
