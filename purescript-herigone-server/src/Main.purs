cd07c35790de864f8ba3464bfe5195fac8064c5d
Ville Peurala <ville.peurala@gmail.com>
2017-09-29 04:48:49 +0300

    Serve static file.

--- purescript-herigone-server/src/Main.purs
+++ purescript-herigone-server/src/Main.purs
@@ -5,18 +5,20 @@ import Node.HTTP as H
 import Node.Stream as S
 import Node.Process (PROCESS)
 
+import Node.FS.Aff (FS, readTextFile)
+
+import Data.Argonaut.Core (stringify)
+import Data.Argonaut.Encode (encodeJson)
+
+import Database.Postgres as PG
+
 import Control.Monad.Aff (Aff, launchAff, runAff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Class (liftEff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Control.Monad.Eff.Exception (EXCEPTION, Error)
 
-import Data.Argonaut.Core (stringify)
-import Data.Argonaut.Encode (encodeJson)
-
 import Data.Maybe as M
-
-import Database.Postgres as PG
 
 import Prelude (Unit, bind, const, discard, map, pure, show, unit, ($), (<>))
 
@@ -35,18 +37,34 @@ listenCallback port = do
   log ("HTTP server listening on port " <> (show port) <> ".")
   pure unit
 
-respondToGET :: forall aff. H.Request -> H.Response -> Aff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE | aff) Unit
+respondToGET :: forall aff. H.Request -> H.Response -> Aff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE, fs :: FS | aff) Unit
 respondToGET request response = do
   liftEff $ log "Responding to GET"
   let responseStream = H.responseAsStream response
       url            = H.requestURL request
-  liftEff $ H.setStatusCode response 200
-  liftEff $ H.setStatusMessage response "OK"
-  liftEff $ H.setHeader response "Connection" "close"
-  liftEff $ H.setHeader response "Transfer-Encoding" "identity"
-  queryResult <- querySelectAllAssociations
-  _ <- liftEff $ S.writeString responseStream UTF8 (stringify (encodeJson queryResult)) (pure unit)
-  liftEff $ S.end responseStream (pure unit)
+  case url of
+    "/associations" -> do
+      liftEff $ H.setStatusCode response 200
+      liftEff $ H.setStatusMessage response "OK"
+      liftEff $ H.setHeader response "Connection" "close"
+      liftEff $ H.setHeader response "Transfer-Encoding" "identity"
+      queryResult <- querySelectAllAssociations
+      _ <- liftEff $ S.writeString responseStream UTF8 (stringify (encodeJson queryResult)) (pure unit)
+      liftEff $ S.end responseStream (pure unit)
+    "/index.html" -> do
+      liftEff $ H.setStatusCode response 200
+      liftEff $ H.setStatusMessage response "OK"
+      liftEff $ H.setHeader response "Connection" "close"
+      liftEff $ H.setHeader response "Transfer-Encoding" "identity"
+      fileContent <- readTextFile UTF8 "static/index.html"
+      _ <- liftEff $ S.writeString responseStream UTF8 fileContent (pure unit)
+      liftEff $ S.end responseStream (pure unit)
+    _ -> do
+      liftEff $ H.setStatusCode response 404
+      liftEff $ H.setStatusMessage response "Not Found"
+      liftEff $ H.setHeader response "Connection" "close"
+      liftEff $ H.setHeader response "Transfer-Encoding" "identity"
+      liftEff $ S.end responseStream (pure unit)
 
 respondToUnsupportedMethod :: forall aff. H.Request -> H.Response -> Aff (http :: H.HTTP, console :: CONSOLE | aff) Unit
 respondToUnsupportedMethod request response = do
@@ -56,7 +74,7 @@ respondToUnsupportedMethod request response = do
   liftEff $ H.setHeader response "Connection" "close"
   liftEff $ S.end responseStream (pure unit)
 
-respond :: forall aff. H.Request -> H.Response -> Aff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE | aff) Unit
+respond :: forall aff. H.Request -> H.Response -> Aff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE, fs :: FS | aff) Unit
 respond request response = do
   let method = H.requestMethod request
       url    = H.requestURL request
@@ -73,19 +91,19 @@ handleRespondSuccess :: forall a eff. a -> Eff (console :: CONSOLE | eff) Unit
 handleRespondSuccess succ =
   log ("Success!")
 
-createServerFunction :: forall eff. H.Request -> H.Response -> Eff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE | eff) Unit
+createServerFunction :: forall eff. H.Request -> H.Response -> Eff (db :: PG.DB, http :: H.HTTP, console :: CONSOLE, fs :: FS | eff) Unit
 createServerFunction request response =
   let canceler = runAff handleRespondError handleRespondSuccess (respond request response)
   in  map (const unit) canceler
 
-asyncMain :: forall aff. Aff (db :: PG.DB, console :: CONSOLE, process :: PROCESS, http :: H.HTTP | aff) Unit
+asyncMain :: forall aff. Aff (db :: PG.DB, console :: CONSOLE, process :: PROCESS, http :: H.HTTP, fs :: FS | aff) Unit
 asyncMain = do
   port <- liftEff getHttpServerPort
   server <- liftEff $ H.createServer createServerFunction
   liftEff $ H.listen server (getListenOptions port) (listenCallback port)
   pure unit
 
-main :: forall eff. Eff (db :: PG.DB, exception :: EXCEPTION, console :: CONSOLE, process :: PROCESS, http :: H.HTTP | eff) Unit
+main :: forall eff. Eff (db :: PG.DB, exception :: EXCEPTION, console :: CONSOLE, process :: PROCESS, http :: H.HTTP, fs :: FS | eff) Unit
 main = do
   canceler <- launchAff asyncMain
   pure unit
