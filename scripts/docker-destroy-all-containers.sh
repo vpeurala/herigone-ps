51672aad31551d6fffa648ab4cd6280bc16ad44c
Ville Peurala <ville.peurala@gmail.com>
2017-10-30 02:55:09 +0200

    Move docker-destroy-* from this project to my .zshrc.

--- scripts/docker-destroy-all-containers.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker ps -a | tail -n +2 | awk '{print $1}' | xargs docker rm -f
--- scripts/docker-destroy-all-images.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-docker images --all | tail -n +2 | awk '{print $3}' | xargs docker rmi -f
