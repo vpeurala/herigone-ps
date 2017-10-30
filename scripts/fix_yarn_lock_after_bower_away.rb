e9089081b1cf3626edc17eddbe798a8618287539
Ville Peurala <ville.peurala@gmail.com>
2017-10-30 14:40:46 +0200

    Script scripts/fix_yarn_lock_after_bower_away.rb works correctly now.
    Submodule purescript-herigone-client fixed.

--- scripts/fix_yarn_lock_after_bower_away.rb
+++ scripts/fix_yarn_lock_after_bower_away.rb
@@ -4,10 +4,11 @@ yarn_lock_file_name = ARGV[0]
 lines = File.readlines(yarn_lock_file_name)
 
 lines.each_with_index {|value, index|
-  match_data = /"@bower_components\/[^#]*#([^"]*)":/.match(value)
+  match_data = /"@bower_components\/[^#]*#([^"]*)":/.match(value.chomp)
+  lines[index] = value.chomp
   lines[index + 1] = "  version \"#{match_data[1]}\"" if match_data
 }
 
 File.open(yarn_lock_file_name, "w") { |f|
-  f.write(lines.join("\n"))
+  f.write(lines.join("\n") + "\n")
 }
