17b4e58286e3cba9b2c13abb3336f0d31ef28768
Ville Peurala <ville.peurala@gmail.com>
2017-11-05 20:45:12 +0200

    Move bower & pulp builds to from package.json/postinstall to docker/build.sh.

--- purescript-herigone-server/docker/build.sh
+++ purescript-herigone-server/docker/build.sh
@@ -3,4 +3,6 @@ set -ex
 PROJECT_ROOT=$(git rev-parse --show-toplevel);
 cd ${PROJECT_ROOT}/purescript-herigone-server;
 yarn install;
+bower install;
+pulp build --to index.js;
 docker build --file=docker/Dockerfile --tag=vpeurala/herigone-ps-server:latest ${PROJECT_ROOT}/purescript-herigone-server
