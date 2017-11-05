4815d7a67b1f9943eca27c1168766f51c3bba0dd
Ville Peurala <ville.peurala@gmail.com>
2017-11-06 01:58:41 +0200

    NGINX front server for Herigone! Passing API requests to the backend still missing.

--- purescript-herigone-nginx/docker/build.sh
+++ purescript-herigone-nginx/docker/build.sh
@@ -2,4 +2,14 @@
 set -ex;
 PROJECT_ROOT=$(git rev-parse --show-toplevel);
 cd ${PROJECT_ROOT}/purescript-herigone-nginx;
+
+if [ -z "${WWW_HERIGONE_COM_KEY}" ]; then echo "Mandatory environment variable WWW_HERIGONE_COM_KEY not set."; exit 1; fi;
+cp ${WWW_HERIGONE_COM_KEY} conf/etc/nginx/keys/
+
+if [ -z "${WWW_HERIGONE_COM_PEM}" ]; then echo "Mandatory environment variable WWW_HERIGONE_COM_PEM not set."; exit 1; fi;
+cp ${WWW_HERIGONE_COM_PEM} conf/etc/nginx/keys/
+
+if [ ! -f ${PROJECT_ROOT}/purescript-herigone-client/herigone.js ]; then echo "Module purescript-herigone-client is not built."; exit 1; fi;
+cp ${PROJECT_ROOT}/purescript-herigone-client/herigone.js static/
+
 docker build --file=docker/Dockerfile --tag=vpeurala/herigone-ps-nginx:latest ${PROJECT_ROOT}/purescript-herigone-nginx;
