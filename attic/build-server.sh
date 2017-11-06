f567a0df7cda5ec5b2b09fa555ae16b3af49f1a8
Ville Peurala <ville.peurala@gmail.com>
2017-11-06 06:52:58 +0200

    Delete attic.

--- attic/build-server.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-set -ex
-PROJECT_ROOT=$(git rev-parse --show-toplevel);
-cd ${PROJECT_ROOT}/purescript-herigone-server;
-yarn install;
-find . -name "*.purs" | xargs purs compile;
-docker build --file=docker/Dockerfile --tag=vpeurala/herigone-ps-server:latest ${PROJECT_ROOT}
